f83e43ee5eb716387bc0f1e23d4700241a5c691b
Cory Buecker <email@corybuecker.com>
2023-02-25 13:15:27 -0600

    --wip-- [skip ci]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'login/new'
   get 'dashboard/index'
 
   root 'dashboard#index'
---
+++ test/controllers/login_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class LoginControllerTest < ActionDispatch::IntegrationTest
+  test 'should get new' do
+    get login_new_url
+    assert_response :success
+  end
+end
