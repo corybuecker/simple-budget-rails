52df3c219bbf4e8baa68676cc0e8bb5448755b04
Cory Buecker <email@corybuecker.com>
2023-04-08 08:56:53 -0500

    Clean up fixtures (#101)
    Add preference tests to User model

--- app/models/preferences_type.rb
+++ app/models/preferences_type.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 class PreferencesType < ActiveModel::Type::Value
-  include ActiveModel::Type::Helpers::Mutable
+  def cast(value)
+    return Preferences.new(value) if value.is_a?(Hash)
+    return Preferences.new(ActiveSupport::JSON.decode(value)) if value.is_a?(String)
 
-  def deserialize(value)
-    Preferences.new(value.is_a?(String) ? ActiveSupport::JSON.decode(value) : value)
+    value
   end
 
   def serialize(value)
-    ActiveSupport::JSON.encode(value)
+    ActiveSupport::JSON.encode(value.attributes)
+  end
+
+  def changed_in_place?(raw_old_value, new_value)
+    ActiveSupport::JSON.decode(raw_old_value) != new_value
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'saves with default preferences' do
+    user = User.new(email: 'test@example.com')
+    assert(user.save)
+    assert(user.reload.preferences.layout == 'automatic')
+  end
+
+  test 'validates preferences' do
+    user = User.new(email: 'test@example.com', preferences: { layout: :not_valid })
+    assert_not(user.valid?)
+    assert(user.errors.full_messages.include?('Preferences Layout is not included in the list'))
+  end
 end
