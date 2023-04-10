22bb738ce467700fc3a82bf13d6a3b402d54871f
Cory Buecker <email@corybuecker.com>
2023-04-10 08:22:37 -0500

    Add Material Design support
    Add favicons from DALL*E

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Simple Budget is a web-based version of the budgeting spreadsheet I have used for 15 years. It's loosely based on envelope-saving.
 
-This particular application is the Ruby on Rails reference version of the Phoenix application. I find it quite useful to build a semi-complex application (more complex than TodoMVC) in any language and framework I am learning. In this case, I use it as a testbed to maintain Hotwire-related skills and experience.
+This particular application is the Ruby on Rails reference version of the Phoenix application. I find it quite useful to build a semi-complex application (more complex than TodoMVC) in any language and framework I am learning. In the case of Rails, I use it as a testbed to maintain Hotwire-related skills and experience.
 
 ## Running Simple Budget
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -9,6 +9,12 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin '@material/web/button/filled-button', to: 'https://ga.jspm.io/npm:@material/web@1.0.0-pre.5/button/filled-button.js'
+pin '@lit/reactive-element', to: 'https://ga.jspm.io/npm:@lit/reactive-element@1.6.1/reactive-element.js'
+pin '@lit/reactive-element/decorators/', to: 'https://ga.jspm.io/npm:@lit/reactive-element@1.6.1/decorators/'
+pin 'lit', to: 'https://ga.jspm.io/npm:lit@2.7.2/index.js'
+pin 'lit-element/lit-element.js', to: 'https://ga.jspm.io/npm:lit-element@3.3.1/lit-element.js'
+pin 'lit-html', to: 'https://ga.jspm.io/npm:lit-html@2.7.2/lit-html.js'
+pin 'lit-html/', to: 'https://ga.jspm.io/npm:lit-html@2.7.2/'
+pin 'lit/', to: 'https://ga.jspm.io/npm:lit@2.7.2/'
+pin 'tslib', to: 'https://ga.jspm.io/npm:tslib@2.5.0/tslib.es6.js'
