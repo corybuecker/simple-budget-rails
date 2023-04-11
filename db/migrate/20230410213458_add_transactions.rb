080064c4a42edd20e195da972712a01986460470
Cory Buecker <email@corybuecker.com>
2023-04-10 19:46:22 -0500

    Fixup indices and clean up unused files

--- db/migrate/20230410213458_add_transactions.rb
+++ db/migrate/20230410213458_add_transactions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddTransactions < ActiveRecord::Migration[7.0]
   def change
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
@@ -8,6 +10,8 @@ class AddTransactions < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
+    # rubocop:disable Rails/NotNullColumn
     add_reference :transactions, :user, type: :uuid, foreign_key: true, index: true, null: false
+    # rubocop:enable Rails/NotNullColumn
   end
 end
--- db/migrate/20230410213644_fix_foreign_key_nullability.rb
+++ db/migrate/20230410213644_fix_foreign_key_nullability.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class FixForeignKeyNullability < ActiveRecord::Migration[7.0]
   def up
     change_column_null :accounts, :user_id, false
